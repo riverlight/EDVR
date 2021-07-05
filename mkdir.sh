8e87fd214935d2f3c609a349a34b13bf58e49392
riverlight <heliang1108@vip.sina.com>
2021-07-05 14:57:37 +0800

    增加mkdir.sh

---
+++ mkdir.sh
@@ -0,0 +1,10 @@
+mkdir datasets
+mkdir datasets/test
+mkdir datasets/test/input
+mkdir datasets/test/target
+mkdir datasets/train
+mkdir datasets/train/input
+mkdir datasets/train/target
+mkdir results
+mkdir results/gt
+mkdir results/output
